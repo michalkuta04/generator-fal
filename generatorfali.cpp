52dd1e284b15354bdef2f81edfb66271061b64dd
Michał Kuta <michal.kuta@orbitvu.com>
2022-12-05 11:09:52 +0100

    WIP Rework dzialania programu. Nie działa.

--- generatorfali.cpp
+++ generatorfali.cpp
@@ -17,66 +17,34 @@ GeneratorFali::~GeneratorFali()
 void GeneratorFali::on_pushButton_clicked()
 {
     QGraphicsView *plot = ui->plot;
+    int plotViewWidth= plot->size().width();
     int plotViewHeight = plot->size().height();
     QGraphicsScene plotScene;
     plot->setScene(&plotScene);
     QPen pen;
     pen.setWidth(4);
-    QLine line;
-    std::vector<double> values;
+    std::vector<std::tuple<double, double>> waveValues;
+    double x1, x2, y1, y2;
     if (ui->pushButton->text() == "Start")
     {
-        QElapsedTimer timer;
-        uint waveFrequency;
-        uint sampleRateHz = ui->spinBoxSampling->value();
-        double sampleRateMs = 1000/sampleRateHz;
-        double elapsedSamplingRateMs = sampleRateMs;
-        double y;
+        std::tuple<double, double> lastValue;
+        QString debugText = "";
+        uint waveFrequency = ui->spinBoxFrequency->value();
+        uint sampleRateMs = ui->spinBoxSampling->value();
+        int i = 0;
         ui->pushButton->setText("Stop");
-        timer.start();
-        // Po wcisnieciu start rozpoczyna petle która czeka na tyle żeby próbkowanie się zgadzało
-        while (ui->pushButton->text() == "Stop")
+        // Oblicza wartosci fali dla kazdego interwału próbkowania i rysuje fale.
+        // TODO: Nic nie działa
+        lastValue = {0, sin(2*M_PI*0*waveFrequency)};
+        do
         {
-            sampleRateHz = ui->spinBoxSampling->value();
-            sampleRateMs = 1000/sampleRateHz;
-            if (timer.elapsed() >= elapsedSamplingRateMs) // Czekaj na próbkowanie
-            {
-                plotScene.clear();
-                waveFrequency = ui->spinBoxFrequency->value();
-                y = sin(2*M_PI*elapsedSamplingRateMs/1000*waveFrequency); // Oblicz wartość fali
-                if (ui->comboBoxShape->currentIndex() == 1) // Jeżeli kwadratowa to przybliż do całości
-                {
-                    if (y > 0)
-                        y = 1;
-                    if (y <= 0)
-                        y = -1;
-                }
-                values.push_back(y);
-                elapsedSamplingRateMs += sampleRateMs; // Kiedy następne próbkowanie
-
-                // Rysowanie fali na GraphicsView
-                // działa strasznie wolno ale działa.
-                if (values.size() >= 2)
-                {
-                    float x1 = 0;
-                    for (auto rit = values.rbegin(); rit < values.rend()-1; rit++)
-                    {
-                        if (x1 > plot->size().width())
-                        {
-                            values.erase((rit+1).base());
-                            break;
-                        }
-                        float x2 = x1 + 300/sampleRateHz;
-                        float y1 = (*rit)*(plotViewHeight/2-2);
-                        float y2 = *(rit+1)*(plotViewHeight/2-2);
-                        line.setLine(x1, y1, x2, y2);
-                        plotScene.addLine(line,pen);
-                        x1 = x2;
-                    }
-                }
-            }
-            QCoreApplication::processEvents(QEventLoop::AllEvents, 1);
-        }
+            i += sampleRateMs;
+            double x = i * plotViewWidth;
+            double y = 0.25 * plotViewHeight;
+            plotScene.addLine(std::get<0>(lastValue), std::get<1>(lastValue), x, y);
+            lastValue = {x, y};
+        } while (i <= 1000);
+        ui->debugLabel->setText(debugText);
     }
     else if (ui->pushButton->text() == "Stop")
     {
